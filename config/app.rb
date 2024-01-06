859cc8e42ed3faa1abeb361d595892394ed67d05
Aotokitsuruya <contact@aotoki.me>
2024-01-06 23:11:15 +0800

    Use config/lambda.rb to replace config/app.rb

--- config/app.rb
+++ config/app.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
-require 'hanami'
+require 'bundler/setup'
+require 'hanami/lambda'
 
 module Stellar
-  class App < Hanami::App
+  class App < Hanami::Lambda::Application
+    delegate 'StellarDailyJob', to: 'daily'
+
+    config.actions.format :json
+    environment :production do
+      require 'secrets'
+
+      Secrets.config.provider = Secrets::Aws
+    end
   end
 end
