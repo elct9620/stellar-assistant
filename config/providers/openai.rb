4f45d7865af07a98522cd140ab4794004aaaf1a3
Aotokitsuruya <contact@aotoki.me>
2024-10-05 22:50:35 +0800

    Add Cloudflare AI Gateway support

--- config/providers/openai.rb
+++ config/providers/openai.rb
@@ -8,8 +8,9 @@ Hanami.app.register_provider :openai do
   start do
     secret = target[:secret]
     access_token = secret.get(:OPENAI_ACCESS_TOKEN)
+    uri_base = secret.get(:OPENAI_URI_BASE)
 
-    client = OpenAI::Client.new(access_token:)
+    client = OpenAI::Client.new(access_token:, uri_base:)
 
     register(:openai, client)
   end
